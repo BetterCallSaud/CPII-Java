15df28a93aa4bd16bccaddaaf5d4b2b08940b57a
SuperbSaud <48680823+SuperbSaud@users.noreply.github.com>
2021-03-30 15:03:49 +0530

    Add files via upload

---
+++ threading-programs/Thread1.java
@@ -0,0 +1,15 @@
+class Thread1 implements Runnable {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread("Java");
+        Thread t2 = new Thread("Python");
+        t1.start();
+        t2.start();
+        System.out.println("Following are the thread names:");
+        System.out.println(t1.getName());
+        System.out.println(t2.getName());
+    }
+
+    @Override
+    public void run() {}
+}
